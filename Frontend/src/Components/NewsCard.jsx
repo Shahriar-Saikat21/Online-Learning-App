004063c69528d307596d2496a646be589e4739fb
Shifty Muhammad Ishmam <sishmam211040@bscse.uiu.ac.bd>
2023-11-12 04:30:17 +0600

    login page done

--- Frontend/src/Components/NewsCard.jsx
+++ Frontend/src/Components/NewsCard.jsx
@@ -3,15 +3,7 @@ import Demo from '../assets/4.jpg'
 const NewsCard = () => {
     return (
         <div className='flex items-center justify-center w-full h-[600px] object-cover px-[60px] pb-9'>
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <img src={"http://localhost:3000/Image/news_image/"+news} alt="news_pic" className='w-full h-full'/>
-=======
-            <img src={"http://localhost:3000/Image/"+news} alt="news_pic" className='w-full h-full'/>
->>>>>>> main
-=======
-            <img src={Demo} alt="news_pic" className='w-full h-full'/>
->>>>>>> parent of cfa8170 (updating)
+
         </div>
     );
 };
