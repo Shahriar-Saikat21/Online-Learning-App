02ff0139f131c92ffc8d26f3d42853c1bc8fb259
saikat <saikatbass@gmail.com>
2023-11-25 16:15:50 +0600

    visitor page popular course design issue fixed

--- Backend/Controller/visitorHomeController.js
+++ Backend/Controller/visitorHomeController.js
@@ -7,3 +7,23 @@ export const displayNews = async (req, res) => {
       res.json(rows);
     });
 };
+
+export const seePopularCourse = async (req, res) => {
+  try{
+    const query = `SELECT COUNT(P.co_id) AS TOTAL,P.co_id AS CourseID,T.co_title,T.co_des,T.co_pic,T.co_price,T.user_name
+    FROM purchase AS P
+    JOIN (SELECT * FROM users AS U
+    JOIN courses AS C
+    WHERE U.user_id = C.co_creator) AS T
+    WHERE P.co_id = T.co_id
+    GROUP BY P.co_id
+    ORDER BY TOTAL DESC
+    LIMIT 5`;
+    connection.query(query, function (err, rows) {
+      if (err) throw err;
+      res.json({success: true,data:rows});
+    });
+  }catch(error){
+    res.json({message: error.message,success: false})
+  }
+};
--- Frontend/src/Components/CourseCardTwo.jsx
+++ Frontend/src/Components/CourseCardTwo.jsx
@@ -1,29 +1,32 @@
-import Pic from "../assets/home-pic.jpg";
-
 import { Link } from "react-router-dom";
 
-const CourseCardTwo = () => {
+const CourseCardTwo = ({ info }) => {
   return (
     <div className="block rounded-lg bg-primary shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
-      <img className="rounded-t-lg" src={Pic} alt="course_pic" />
+      <div className="md:relative md:w-[270px] md:h-[200px]">
+        <img
+          className="rounded-t-lg md:absolute inset-0 w-full h-full object-cover"
+          src={"http://localhost:3000/Image/" + info.co_pic}
+          alt="course_pic"
+        />
+      </div>
 
       <div className="p-6">
-        <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-          Big Data Analysis
+        <div className=" h-16 overflow-hidden">
+        <h5 className="mb-2 text-xl font-medium leading-tight text-white ">
+          {info.co_title}
         </h5>
-        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-          Some quick example text to build on the card title and make up the
-          bulk of the content.
+        </div>
+        <p className="mb-4 text-base text-white italic">{info.co_des}</p>
+        <p className="mb-4 text-base text-white ">
+          Created by : {info.user_name}
         </p>
-        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-          Created by : Shahriar Saikat
-        </p>
-        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-          Price : 1000 BDT
+        <p className="mb-4 text-base text-white ">
+          Price : {info.co_price} BDT
         </p>
 
         <Link
-          to="/course/:courseId"
+          to={"/course/" + info.CourseID}
           className="inline-block rounded bg-white px-6 pb-2 pt-2.5 text-lg font-medium leading-normal text-[#192655] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
         >
           Show Detail
